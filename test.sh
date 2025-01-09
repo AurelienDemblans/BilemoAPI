add06c1ca69e22279254ee19aa774775687a5c0c
DESKTOP-9MTB132\Aurel <demblans.dev@gmail.com>
2025-01-09 20:06:08 +0100

    install and config lexik jwt

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-curl -X POST -H "Content-Type: application/json" http://127.0.0.1:8000/api/login_check --data {\"username\":\"SFR\",\"password\":\"testSFR\"}
+curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" http://127.0.0.1:8000/api/login_check --data '{"username":"SFR","password":"testSFR"}'
