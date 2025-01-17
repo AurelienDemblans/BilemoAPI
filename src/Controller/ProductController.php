c12bf7a4499a48ed8f3d6b690dbad6fb62bf5452
DESKTOP-9MTB132\Aurel <demblans.dev@gmail.com>
2025-01-17 11:01:58 +0100

    implement cache

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,16 +10,29 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class ProductController extends AbstractController
 {
     //TODO
     #[Route('/api/products', name: 'app_products', methods: Request::METHOD_GET) ]
-    public function index(ProductRepository $productRepository, SerializerInterface $serializer, Request $request): JsonResponse
-    {
+    public function index(
+        ProductRepository $productRepository,
+        SerializerInterface $serializer,
+        Request $request,
+        TagAwareCacheInterface $cachePool
+    ): JsonResponse {
         $page = $request->get('page', 1);
         $limit = $request->get('limit', 3);
-        $productList = $productRepository->findAllWithPagination($page, $limit);
+
+        $idCache = "getAllProducts-" . $page . "-" . $limit;
+
+        // $productList = $productRepository->findAllWithPagination($page, $limit);
+        $productList = $cachePool->get($idCache, function (ItemInterface $item) use ($productRepository, $page, $limit) {
+            $item->tag("ProductsCache");
+            return $productRepository->findAllWithPagination($page, $limit);
+        });
 
         $jsonProductList = $serializer->serialize($productList, 'json');
 
